4e3b281d9b81d600a342d95840c15889fd558ceb
HarinKhakhi <k.harin2306@gmail.com>
2023-09-22 18:01:24 -0700

    Submission

--- src/1_partition.cpp
+++ src/1_partition.cpp
@@ -1,5 +1,21 @@
 #include "searching_algorithms.h"
 
 int partition(vector<int>& arr, int start, int end){
-    return INT_MAX;
+    int pivotIdx = start + rand() % (end-start+1);
+    int pivot = arr[pivotIdx];
+    swap(arr[pivotIdx], arr[end]);
+    pivotIdx = end;
+    int i = start -1;
+ 
+    for(int j=start; j<=end-1; j++)
+    {
+        if(arr[j] <= pivot)
+        {
+            i = i+1;
+            swap(arr[i], arr[j]);
+        }
+    }
+ 
+    swap(arr[i+1], arr[pivotIdx]);
+    return i+1;
 }
--- src/2_selection.cpp
+++ src/2_selection.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int randomized_select(vector<int> nums, int k, int start, int end){
-    return INT_MAX;
+    if(start > end) return -1;
+
+    int pos = partition(nums, start, end);
+
+    int left_size = pos - start;
+    if(k < left_size)
+        return randomized_select(nums, k, start, pos-1);
+    else if(k > left_size)
+        return randomized_select(nums, k-left_size-1, pos+1, end);
+
+    return nums[pos];
 }
